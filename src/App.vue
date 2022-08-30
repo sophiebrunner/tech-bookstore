12fdde936e9966c714709ab9c8572eaa87c11e18
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-08-30 15:08:49 +0200

    add, register and use  BookList and HeaderLayout component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <LayoutHeader />
+  <BookList />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    BookList,
+    LayoutHeader,
   },
 };
+import BookList from "./components/BookList.vue";
+import LayoutHeader from "./components/LayoutHeader.vue";
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
