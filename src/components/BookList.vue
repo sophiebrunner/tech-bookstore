d3f702c196c4f4040b1136ae89b8b005abc690bd
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-09-06 13:53:10 +0200

    feat: add fetch-request to get data from bookmonkey-api

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -71,40 +71,44 @@ export default {
   data() {
     return {
       books: [
-        {
-          title: "Practical Rust Web Projects",
-          isbn: "9781484265888",
-          author: "Shing Lyu",
-          publisher: "Apress",
-          price: "$28.75",
-          numPages: 256,
-        },
-        {
-          title: "Using WebPagetest",
-          isbn: "9781491902592",
-          author: "Rick Viscomi, Andy Davies, Marcel Duran",
-          publisher: "O'Reilly Media",
-          price: "$25.80",
-          numPages: 214,
-        },
-        {
-          title: "Web Scraping with Python",
-          isbn: "9781491910290",
-          author: "Ryan Mitchell",
-          publisher: "O'Reilly Media",
-          price: "$14.00",
-          numPages: 256,
-        },
-        {
-          title: "High Performance Mobile Web",
-          isbn: "9781491912553",
-          author: "Maximiliano Firtman",
-          publisher: "O'Reilly Media",
-          price: "$7.00",
-          numPages: 326,
-        },
+        // {
+        //   title: "Practical Rust Web Projects",
+        //   isbn: "9781484265888",
+        //   author: "Shing Lyu",
+        //   publisher: "Apress",
+        //   price: "$28.75",
+        //   numPages: 256,
+        // },
+        // {
+        //   title: "Using WebPagetest",
+        //   isbn: "9781491902592",
+        //   author: "Rick Viscomi, Andy Davies, Marcel Duran",
+        //   publisher: "O'Reilly Media",
+        //   price: "$25.80",
+        //   numPages: 214,
+        // },
+        // {
+        //   title: "Web Scraping with Python",
+        //   isbn: "9781491910290",
+        //   author: "Ryan Mitchell",
+        //   publisher: "O'Reilly Media",
+        //   price: "$14.00",
+        //   numPages: 256,
+        // },
+        // {
+        //   title: "High Performance Mobile Web",
+        //   isbn: "9781491912553",
+        //   author: "Maximiliano Firtman",
+        //   publisher: "O'Reilly Media",
+        //   price: "$7.00",
+        //   numPages: 326,
+        // },
       ],
     };
+  },
+  async created() {
+    const response = await fetch("http://localhost:4730/books?_limit=10");
+    this.books = await response.json();
   },
   methods: {
     onToggleBookmark(isbn) {
